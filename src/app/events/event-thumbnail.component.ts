abb77399985a418fa4975b42bd911f160a8af5cf
SteveMustanski <steve.mustanski@gmail.com>
2021-02-10 11:36:35 -0500

    added styling to event thumbnail

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,7 +2,13 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
-  templateUrl: './event-thumbnail-component.html'
+  templateUrl: './event-thumbnail-component.html',
+  styles: [
+    `
+    .pad-left { margin-left: .5rem;}
+    .well div {color: #bbb;}
+    `
+  ]
 })
 
 export class EventThumbnailComponent  {
