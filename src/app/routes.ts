42abcaa91e17ebc59d37a21b8376eb522c22559f
SteveMustanski <steve.mustanski@gmail.com>
2021-02-13 16:05:07 -0500

    added user module

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -12,5 +12,6 @@ export const appRoutes: Routes = [
   { path: 'events', component: EventsListComponent, resolve: {events:EventListResolver}},
   { path: 'events/:id', component: EventDetailsComponent},
   { path: '404', component: Error404Component, canActivate: [EventRouteActivator]},
-  { path: '', redirectTo: '/events', pathMatch: 'full'}
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  { path: 'user', loadChildren: './user/user.module#UserModule'}
 ]
