ec5b4fb37b86e7a53b1dd3e88702c35600ab0dbe
SteveMustanski <steve.mustanski@gmail.com>
2021-02-13 11:48:33 -0500

    added router, routes, and hard coded details route

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-app',
   template: `
   <nav-bar></nav-bar>
-  <events-list></events-list>
+  <router-outlet></router-outlet>
   `
 })
 export class EventsAppComponent {
-  title = 'ng-fundamentals';
+  title = 'app';
 }
