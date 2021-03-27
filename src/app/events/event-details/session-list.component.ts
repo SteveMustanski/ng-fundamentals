a0996deade9b4451417f9c9d1bc4c77b5b7b24d9
SteveMustanski <steve.mustanski@gmail.com>
2021-03-27 17:11:34 -0400

    added filter sessions

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared';
 
 @Component({
@@ -6,11 +6,29 @@ import { ISession } from '../shared';
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.css']
 })
-export class SessionListComponent implements OnInit {
-  @Input() sessions:ISession[]
+export class SessionListComponent implements OnChanges {
+  @Input() sessions:ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[] = [];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges() {
+    if(this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter) {
+    if(filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      })
+    }
   }
 
 }
