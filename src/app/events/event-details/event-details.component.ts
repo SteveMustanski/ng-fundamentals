ab35e09b253b4e1614200610eae5840b3e74f2f8
SteveMustanski <steve.mustanski@gmail.com>
2021-04-10 11:45:36 -0400

    fixed bug in event details for resetting the route

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from "../shared/event.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 import { IEvent, ISession } from "../shared/index";
 
 @Component({
@@ -17,7 +17,11 @@ export class EventDetailsComponent implements OnInit {
   constructor(private eventService:EventService, private route:ActivatedRoute) { }
 
   ngOnInit() {
-      this.event = this.eventService.getEvent(+this.route.snapshot.params['id']) // the '+' casts the id to a number
+
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params['id']);
+      this.addMode = false;
+    })
   }
 
   addSession() {
